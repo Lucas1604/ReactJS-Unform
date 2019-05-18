f367361623683915a0ee9a9d7bf2426338dadb4d
Lucas Barcelos <lucasdesignerlt@gmail.com>
2019-05-18 18:42:42 -0300

    Commit frontend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Form, Input, Scope } from "@rocketseat/unform";
+import api from './services/api';
+import './index.css';
 
 function App() {
+  function handleSubmit(data) {
+    console.log(data);
+
+    const ar = api.get('usuario');
+
+    console.log(ar);
+
+    api.post('fazenda', data)
+
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="login-wrapper">
+      <h1 className="title">Cadastro de Fazenda</h1>
+      <Form onSubmit={handleSubmit}>
+        <Input name="codigo"  placeholder="Código"/> <br/>
+        <Input name="nome"  placeholder="Nome"/> <br/>
+
+
+        <h3 className="title">Fazenda:</h3>
+        <Scope path="fazenda">
+          <Input name="talhao"  placeholder="Talhão"/> <br/>
+          <Input name="propriedade"  placeholder="Propriedade" />
+        </Scope>
+        <br />
+        
+        <h3 className="title">Medidas:</h3>
+        <Scope path="medidas">
+          <Input name="hectares"  placeholder="Hectares"/> <br/>
+          <Input name="metros"  placeholder="Metros" /> <br/>
+          <Input name="ares"  placeholder="Ares" /> 
+        </Scope>
+
+        <h3 className="title">Plantas:</h3>
+        <Scope path="plantas">
+          <Input name="graos"  placeholder="Grãos"/> <br/>
+          <Input name="cafe"  placeholder="Café" /> <br/>
+          <Input name="cana"  placeholder="Cana" /> 
+        </Scope>
+
+
+        <button type="submit">Salvar</button>
+      </Form>
     </div>
+   
   );
 }
 
